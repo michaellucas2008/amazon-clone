ca53063659c6240c7702ad076807872ec9e947da
asanez2008 <141792618+asanez2008@users.noreply.github.com>
2024-02-28 17:18:39 -0500

    Add files via upload

---
+++ scripts/checkout.js
@@ -0,0 +1,4 @@
+import {displayOrderSummary} from './checkout/orderSummary.js';
+import {displayPaymentSummary} from './checkout/paymentSummary.js';
+displayOrderSummary();
+displayPaymentSummary();
